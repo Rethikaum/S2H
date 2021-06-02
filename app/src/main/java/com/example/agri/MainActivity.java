a6e074dfa7e5d23d1251758078098e0b2ae50e4e
Deetchi <deetchivanan0109@gmail.com>
2021-06-02 20:24:03 +0530

    initial commit

--- app/src/main/java/com/example/agri/MainActivity.java
+++ app/src/main/java/com/example/agri/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                    Intent home = new Intent(MainActivity.this,Home_page.class);
+                    Intent home = new Intent(MainActivity.this,Login.class);
                     startActivity(home);
                     finish();
             }
