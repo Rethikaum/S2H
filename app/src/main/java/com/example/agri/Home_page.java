614e4658354ba9614dfe6669b76f0d439ee85b0a
Deetchi <deetchivanan0109@gmail.com>
2021-06-04 18:30:20 +0530

    initial commit

--- app/src/main/java/com/example/agri/Home_page.java
+++ app/src/main/java/com/example/agri/Home_page.java
@@ -2,7 +2,13 @@ package com.example.agri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Home_page extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class Home_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
     }
+
+
 }
